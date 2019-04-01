9acc2d6aac55cadf59e12200770a1b63913368a0
sdsy888 <7611912+sdsy888@users.noreply.github.com>
2019-04-01 17:12:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Just for installation some necessary packages in the fresh new Ubuntu system
---
+++ installation-scripts.sh
@@ -0,0 +1,10 @@
+sudo apt update
+sudo apt upgrade
+# 可以自动安装deb相关依赖
+sudo apt-get install gdebi-core
+sudo apt-get install gksu
+sudo apt-get install gnome-icon-theme
+sudo apt-get install gdebi
+
+# 读取exfat格式
+sudo apt-get install exfat-utils
